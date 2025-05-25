c00ea5f83fded806578de2ac6a79091951956ec9
CodingIsFun1010 <167133428+CodingIsFun1010@users.noreply.github.com>
2025-05-24 21:50:41 -0400

    Update and rename bottle.png to bottle_image.py

---
+++ bottle_image.py
@@ -0,0 +1,8 @@
+from PIL import Image, ImageDraw, ImageFilter
+
+def create_glowing_bottle(filename='bottle.png'):
+    # (script content here - same as before)
+
+if __name__ == "__main__":
+    create_glowing_bottle()
+    print("bottle.png created!")
